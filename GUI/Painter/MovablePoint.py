65dd20d73a3a29c8aea6fba1568c18dc51bcd21d
Ruslan Aukhadyev <78228760+Excavator0@users.noreply.github.com>
2024-11-22 21:53:49 +0300

    добавил редактирование лифтов/лестниц, добавил показ квартир

--- GUI/Painter/MovablePoint.py
+++ GUI/Painter/MovablePoint.py
@@ -3,16 +3,20 @@ from PyQt5.QtWidgets import QGraphicsEllipseItem
 
 
 class MovablePoint(QGraphicsEllipseItem):
-    def __init__(self, x, y, radius, point_id, parent_polygon, editor):
+    def __init__(self, x, y, radius, point_id, parent_polygon, editor, preview=False):
         super().__init__(-radius, -radius, 2 * radius, 2 * radius)
         self.setPos(x, y)
-        self.setFlags(QGraphicsEllipseItem.ItemIsMovable | QGraphicsEllipseItem.ItemIsSelectable)
+        if not preview:
+            self.setFlags(QGraphicsEllipseItem.ItemIsMovable | QGraphicsEllipseItem.ItemIsSelectable)
+        else:
+            self.setFlag(QGraphicsEllipseItem.ItemIsSelectable, False)
         self.default_brush = Qt.blue
         self.setBrush(self.default_brush)
         self.selected_brush = Qt.lightGray
         self.radius = radius
         self.point_id = point_id
         self.editor = editor
+        self.preview = preview
         self.parent_polygon = parent_polygon
 
     def paint(self, painter, option, widget=None):
@@ -35,4 +39,5 @@ class MovablePoint(QGraphicsEllipseItem):
     def mouseMoveEvent(self, event):
         """ Update connected lines in real-time as the point moves. """
         super().mouseMoveEvent(event)
-        self.parent_polygon.updatePolygon()
+        if not self.preview:
+            self.parent_polygon.updatePolygon()
